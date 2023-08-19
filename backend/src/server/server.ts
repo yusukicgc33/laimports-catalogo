0012a6aeb49a42c55c4b12a3918a3998609c26d6
LestterX <lgs.alves@hotmail.com>
2023-08-19 15:16:55 -0300

    feat: ...views dir public

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -23,6 +23,7 @@ server.use(cors({
         'DELETE'
     ]
 }))
+server.use(express.static(path.resolve(__dirname, 'views'))) //DEIXANDO A PASTA VIEWS PUBLICA || Falha de sugurança
 server.use(express.json())
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(routes)
