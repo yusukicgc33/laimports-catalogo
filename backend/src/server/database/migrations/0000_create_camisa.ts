2de0c10b5886f7513e4b42c09c33cbffd77b9d7c
LestterX <lgs.alves@hotmail.com>
2023-08-20 02:59:53 -0300

    feat: remove img limit from db and desc 20 limit

--- backend/src/server/database/migrations/0000_create_camisa.ts
+++ backend/src/server/database/migrations/0000_create_camisa.ts
@@ -7,9 +7,9 @@ export async function up(knex: Knex) {
             table.bigIncrements('id').primary().index();
             table.string('liga', 50).index().notNullable;
             table.string('selecao', 50).index().unique().notNullable;
-            table.string('descricao', 50).notNullable;
+            table.string('descricao', 20).notNullable;
             table.string('custo', 50).notNullable;
-            table.string('imagem', 50).notNullable;
+            table.string('imagem').notNullable;
         })
         .then(() => {
             console.log(`[# Server] Tabela camisa criada`)
